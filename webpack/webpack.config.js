3c267217750050983bd024aadb7a81204831b593
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-19 11:16:34 -0300

    .scss files, images and Webserver

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,25 +1,45 @@
-const webpack =  require('webpack')
+const modoDev = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode:'development',
-    entry:'./src/principal.js',
-    output:{
-        filename:'pricipal.js',
-        path:__dirname + '/public'
+    mode: modoDev ? 'development' : 'production',
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
     },
-    plugins:[
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
         new MiniCssExtractPlugin({
-            filename:'estilo.css'
+            filename: "estilo.css"
         })
     ],
-    module:{
-        rules:[{
-            test:/\.css$/,
-            use:[
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
                 MiniCssExtractPlugin.loader,
-                'css-loader'
+                'css-loader', 
+                'sass-loader',
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
