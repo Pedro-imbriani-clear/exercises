86c684fe3e95040eebab1d2098ba4609fd884bbf
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-19 12:55:18 -0300

    component children

--- react/exercicios-react/src/index.js
+++ react/exercicios-react/src/index.js
@@ -4,6 +4,7 @@ import Primeiro from './componentes/Primeiro'
 import BomDia from './componentes/BomDia'
 import { BoaTarde, BoaNoite } from './componentes/multiplos'
 import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
 
 
 ReactDOM.render(
@@ -12,6 +13,7 @@ ReactDOM.render(
         <BoaTarde nome='Barbara'/>
         <BoaNoite nome='Pedro'/>
         <Saudacao tipo='Bom diaa' nome='Joao'/>
+        <Pai nome="Paulo" sobrenome='Silva'/>
        
     </div>
     , document.getElementById('root'))
