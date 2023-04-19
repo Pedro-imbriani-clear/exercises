f65727c7d85c67791a91cf4f3e16e9a19fa25f73
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-19 12:33:32 -0300

    multiple components  and component class #01

--- react/exercicios-react/src/index.js
+++ react/exercicios-react/src/index.js
@@ -2,5 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Primeiro from './componentes/Primeiro'
 import BomDia from './componentes/BomDia'
+import { BoaTarde, BoaNoite } from './componentes/multiplos'
+import Saudacao from './componentes/Saudacao'
 
-ReactDOM.render(<BomDia nome='guilherme'/>, document.getElementById('root'))
+
+ReactDOM.render(
+    <div>
+        <BomDia nome='guilherme' />
+        <BoaTarde nome='Barbara'/>
+        <BoaNoite nome='Pedro'/>
+        <Saudacao tipo='Bom diaa' nome='Joao'/>
+       
+    </div>
+    , document.getElementById('root'))
