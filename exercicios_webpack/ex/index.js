c1fc585a4bb3a5553b7e3ca700f4721e548a0400
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-25 11:52:58 -0300

    es2015

--- exercicios_webpack/ex/index.js
+++ exercicios_webpack/ex/index.js
@@ -1,4 +1,4 @@
-const logger = require('./logger')
+import Pessoa from "./pessoa"
 
-logger.info('Usando o padrao CommnonJS!')
-console.log('webpack')
+const pessoa = new Pessoa('Pedro2')
+console.log(pessoa.toString())
