25803964af3277b3603d8ef8da3e16fca084e0a3
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-26 11:04:49 -0300

    relationship between components

---
+++ exercicios-react2/src/components/FamiliaSilva.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Menbro from "./Menbro";
+
+export default props=>
+    <div>
+        <Menbro nome='Rafael' sobrenome='Silva'/>
+        <Menbro nome='pedro' sobrenome='Silva'/>
+        <Menbro nome='Rafaela' sobrenome='Silva'/>
+        <Menbro nome='Carla' sobrenome='Silva'/>
+    </div>
--- exercicios-react2/src/index.js
+++ exercicios-react2/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import Primeiro from "./components/PrimeiroComponent";
 import { CompA,CompB } from "./components/Componet2";
 import Multi from "./components/Multi";
+import FamiliaSilva from "./components/FamiliaSilva";
+import Familia from "./components/Familia";
+import Menbro from "./components/Menbro";
 
 const element = document.getElementById('root')
 ReactDOM.render(
@@ -11,5 +14,17 @@ ReactDOM.render(
         <CompA value='ola sou A'/>
         <CompB value='ola sou B'/>
         <Multi/>
+        <FamiliaSilva/>
+        <hr/>
+        <Familia>
+            <Menbro nome='Pedro' sobrenome='Imbriani'/>
+            <Menbro nome='Aline' sobrenome='Imbriani'/>
+            <Menbro nome='Isadora' sobrenome='Imbriani'/>
+        </Familia>
+                <Familia>
+            <Menbro nome='Claudia' sobrenome='Pereira'/>
+            <Menbro nome='Jeff' sobrenome='Pereira'/>
+            <Menbro nome='Ricardo' sobrenome='Pereira'/>
+        </Familia>
     </div>
 ,element)
