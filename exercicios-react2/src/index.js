a5ce825898b89326e5ee6ec46770c8039c54082e
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-26 10:32:57 -0300

    multiple elements

--- exercicios-react2/src/index.js
+++ exercicios-react2/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Primeiro from "./components/PrimeiroComponent";
+import { CompA,CompB } from "./components/Componet2";
+import Multi from "./components/Multi";
 
 const element = document.getElementById('root')
 ReactDOM.render(
     <div>
         <Primeiro value='bom dia'/>
+        <CompA value='ola sou A'/>
+        <CompB value='ola sou B'/>
+        <Multi/>
     </div>
 ,element)
